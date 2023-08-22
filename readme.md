85330b80aaa1ff9ee0df24b9fcdf79fc12738aec
jose miguel mut lopez <jose.mut@intrasonics.com>
2023-08-22 13:07:30 +0100

    support "at <time>

--- readme.md
+++ readme.md
@@ -1,15 +1,53 @@
 
 # Remind
 
-Install:
+A command line tool to set reminders.
+
+Reminders will create a pop-up in MacOs with a given message after a given delay.
+Reminders are non-persistent after a reboot.
+
+## Install:
+This will install the `remind` command so that it's available in your terminal.
 ```
 cargo install --force --path .
 ```
 
-Example usages:
+## Example usages:
+```
+$ remind me to do something in 5 minutes
+$ remind me in 1 hour that I have a meeting
+$ remind cycling in 30 minutes
+$ remind me to stop working at 17:30
+```
 
+## Grammar
 ```
-remind me to do something in 5 minutes
-remind me in 1 hour I have a meeting
-remind go out in 30 minutes
+command = 'remind' 'me'? (action | action time | time action)
+action = 'to'? message | 'that'? message
+message = string
+time = 'in' number time_unit | 'at' hour | 'at' minute
+time_unit = 'second' | 'seconds' | 'minute' | 'minutes' | 'hour' | 'hours'
+hour = number ':' number
+minute = number ':' number ':' number
 ```
+
+### Meta-grammar
+The grammar above is written according to the following custom meta-grammar:
+
+- literals: strings within single quotes, e.g. 'some literal'
+- rule names: non-quoted words that don't contain spaces, e.g. rule1
+- concatenation: rules or literals separated by spaces, e.g. rule1 rule2
+- alternatives: rules or literals separated by a vertical line, e.g. rule1 | rule2
+- rule 'string': a sequence of characters, e.g. some string
+- rule 'number': a sequence of digits, e.g. 123
+
+## Semantic rules
+
+- if no time is specified, 0 seconds is used.
+
+## How the pop-up works
+
+The pop-up is created using AppleScript, which is executed using the `osascript` command.
+Something like `osascript -e 'display alert "Hello World"'` will create a pop-up with the message 'Hello World'.
+
+I could have used `display notification`, but the notification only appears for some seconds, which might be easily missed.
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use std::process::Command;
+use std::time::Duration;
+use chrono::NaiveTime;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    let result = try_parse(&args.iter().map(String::as_str).collect::<Vec<_>>());
+    let args_ref:Vec<&str> = args.iter().map(String::as_str).collect();
+    let result = try_parse(&args_ref, chrono::Local::now().time());
     match result {
         Err(e) => println!("Error: {}", e),
         Ok(reminder) => {
@@ -25,11 +28,11 @@ const ACTION_MARKERS: [&str; 2] = ["to", "that"];
 
 #[derive(Debug, PartialEq)]
 struct Reminder {
-    time: std::time::Duration,
+    time: Duration,
     action: String,
 }
 
-fn try_parse(words: &[&str]) -> Result<Reminder, String> {
+fn try_parse(words: &[&str], now: NaiveTime) -> Result<Reminder, String> {
     let mut i = 1;
     if words.len() <= i {
         return Err("Expected an action, 'in <time interval>', or 'me'".to_string());
@@ -41,7 +44,20 @@ fn try_parse(words: &[&str]) -> Result<Reminder, String> {
         return Err("Expected an action or 'in <time interval>'".to_string());
     }
     if words[i] == "in" {
-        let time = parse_time(words, &mut i)?;
+        let time = parse_time_diff(words, &mut i)?;
+        if words.len() <= i {
+            return Err("Expected an action".to_string());
+        }
+        if ACTION_MARKERS.contains(&words[i]) {
+            i += 1;
+        }
+        let action = words[i..].join(" ");
+        Ok(Reminder {
+            time,
+            action,
+        })
+    } else if words[i] == "at" {
+        let time = parse_time(words, &mut i, now)?;
         if words.len() <= i {
             return Err("Expected an action".to_string());
         }
@@ -60,35 +76,41 @@ fn try_parse(words: &[&str]) -> Result<Reminder, String> {
         if words.len() <= i {
             return Err("Expected an action".to_string());
         }
-        let time_index_opt = words.iter().enumerate().rfind(|w| w.1 == &"in").map_or(None, |w| Some(w.0));
-        return match time_index_opt {
-            Some(mut time_index) => {
-                let action = words[i..time_index].join(" ");
-                let time = parse_time(&words, &mut time_index)?;
-                Ok(Reminder {
-                    time,
-                    action,
-                })
-            },
-            None => {
-                let action = words[i..].join(" ");
-                Ok(Reminder {
-                    time: std::time::Duration::from_secs(0),
-                    action,
-                })
-            }
-        };
+        let time_diff_index_opt = words.iter().enumerate().rfind(|w| w.1 == &"in").map_or(None, |w| Some(w.0));
+        if let Some(mut time_index) = &time_diff_index_opt {
+            let action = words[i..time_index].join(" ");
+            let time = parse_time_diff(&words, &mut time_index)?;
+            return Ok(Reminder {
+                time,
+                action,
+            });
+        }
+
+        let time_index_opt = words.iter().enumerate().rfind(|w| w.1 == &"at").map_or(None, |w| Some(w.0));
+        if let Some(mut time_index) = &time_index_opt {
+            let action = words[i..time_index].join(" ");
+            let time = parse_time(&words, &mut time_index, now)?;
+            return Ok(Reminder {
+                time,
+                action,
+            });
+        }
+        let action = words[i..].join(" ");
+        return Ok(Reminder {
+            time: Duration::from_secs(0),
+            action,
+        });
     }
 }
 
-fn parse_time(words: &[&str], i: &mut usize) -> Result<std::time::Duration, String> {
+fn parse_time_diff(words: &[&str], i: &mut usize) -> Result<Duration, String> {
     if words.len() <= *i {
         return Err("Expected a time interval".to_string());
     }
     if words[*i] == "in" {
         *i += 1;
     } else {
-        return Ok(std::time::Duration::from_secs(0));
+        return Ok(Duration::from_secs(0));
     }
     if words.len() <= *i {
         return Err("Expected a time interval".to_string());
@@ -102,76 +124,145 @@ fn parse_time(words: &[&str], i: &mut usize) -> Result<std::time::Duration, Stri
         return Err("Expected a time unit".to_string());
     }
     let time = match words[*i] {
-        "second" | "seconds" => std::time::Duration::from_secs(time),
-        "minute" | "minutes" => std::time::Duration::from_secs(time * 60),
-        "hour" | "hours" => std::time::Duration::from_secs(time * 60 * 60),
+        "second" | "seconds" => Duration::from_secs(time),
+        "minute" | "minutes" => Duration::from_secs(time * 60),
+        "hour" | "hours" => Duration::from_secs(time * 60 * 60),
         _ => return Err("Expected a time unit".to_string()),
     };
     *i += 1;
     Ok(time)
 }
 
+fn parse_time(words: &[&str], i: &mut usize, now: NaiveTime) -> Result<Duration, String> {
+    if words.len() <= *i {
+        return Err("Expected a time".to_string());
+    }
+    if words[*i] == "at" {
+        *i += 1;
+    } else {
+        return Ok(Duration::from_secs(0));
+    }
+    if words.len() <= *i {
+        return Err("Expected a time".to_string());
+    }
+    let time = match NaiveTime::parse_from_str(words[*i], "%H:%M") {
+        Ok(t) => t,
+        Err(_) => {
+            match NaiveTime::parse_from_str(words[*i], "%H:%M:%S") {
+                Ok(t) => t,
+                Err(_) => return Err("Expected a time".to_string()),
+            }
+        },
+    };
+    *i += 1;
+    let time = time.signed_duration_since(now);
+    if time.num_seconds() < 0 {
+        return Err("Time should not be in the past".to_string());
+    }
+    Ok(Duration::from_secs(time.num_seconds() as u64))
+}
+
+
 #[cfg(test)]
 mod tests {
+    use chrono::NaiveTime;
     use super::*;
+
+    fn now() -> NaiveTime {
+        return NaiveTime::from_hms_opt(13, 51, 30).unwrap();
+    }
 
     #[test]
     fn test_parse_empty() {
-        let result = try_parse(&[]);
-        assert_eq!(result.is_ok(), false);
+        let result = try_parse(&[], now());
+        assert!(result.is_err());
     }
+
     #[test]
     fn test_parse_only_command() {
-        let result = try_parse(&["remind"]);
-        assert_eq!(result.is_ok(), false);
+        let result = try_parse(&["remind"], now());
+        assert!(result.is_err());
     }
 
     fn to_slice(s: &str) -> Vec<&str> {
         s.split_ascii_whitespace().collect::<Vec<&str>>()
     }
+
     #[test]
     fn test_parse_only_me() {
-        let result = try_parse(&to_slice("remind me"));
-        assert_eq!(result.is_ok(), false);
+        let result = try_parse(&to_slice("remind me"), now());
+        assert!(result.is_err());
     }
+
     #[test]
     fn test_parse_only_action() {
-        let result = try_parse(&to_slice("remind work"));
+        let result = try_parse(&to_slice("remind work"), now());
         assert_eq!(result, Ok(Reminder {
-            time: std::time::Duration::from_secs(0),
+            time: Duration::from_secs(0),
             action: "work".to_string(),
         }));
     }
+
     #[test]
     fn test_parse_time_and_action() {
-        let result = try_parse(&to_slice("remind in 5 minutes to ask"));
+        let result = try_parse(&to_slice("remind in 5 minutes to ask"), now());
         assert_eq!(result, Ok(Reminder {
-            time: std::time::Duration::from_secs(5 * 60),
+            time: Duration::from_secs(5 * 60),
             action: "ask".to_string(),
         }));
     }
+
     #[test]
     fn test_parse_to_action_and_time() {
-        let result = try_parse(&to_slice("remind to ask in 5 minutes"));
+        let result = try_parse(&to_slice("remind to ask in 5 minutes"), now());
         assert_eq!(result, Ok(Reminder {
-            time: std::time::Duration::from_secs(5 * 60),
+            time: Duration::from_secs(5 * 60),
             action: "ask".to_string(),
         }));
     }
+
     #[test]
     fn test_parse_me_and_action_and_time() {
-        let result = try_parse(&to_slice("remind me to ask something in 5 minutes"));
+        let result = try_parse(&to_slice("remind me to ask something in 5 minutes"), now());
         assert_eq!(result, Ok(Reminder {
-            time: std::time::Duration::from_secs(5 * 60),
+            time: Duration::from_secs(5 * 60),
             action: "ask something".to_string(),
         }));
     }
+
     #[test]
     fn test_parse_action_and_time() {
-        let result = try_parse(&to_slice("remind closing something in 5 minutes"));
+        let result = try_parse(&to_slice("remind closing something in 5 minutes"), now());
         assert_eq!(result, Ok(Reminder {
-            time: std::time::Duration::from_secs(5 * 60),
+            time: Duration::from_secs(5 * 60),
             action: "closing something".to_string(),
         }));
+    }
+
+    #[test]
+    fn test_parse_action_and_at_time() {
+        let now = NaiveTime::from_hms_opt(13, 51, 30).unwrap();
+        let result = try_parse(&to_slice("remind me to do something at 14:10"), now);
+        assert_eq!(result, Ok(Reminder {
+            time: Duration::from_secs(18 * 60 + 30),
+            action: "do something".to_string(),
+        }));
+    }
+
+    #[test]
+    fn test_parse_action_and_at_time_seconds() {
+        let now = NaiveTime::from_hms_opt(13, 51, 30).unwrap();
+        let result = try_parse(&to_slice("remind me to do something at 14:10:31"), now);
+        assert_eq!(result, Ok(Reminder {
+            time: Duration::from_secs(19 * 60 + 1),
+            action: "do something".to_string(),
+        }));
+    }
+
+    #[test]
+    fn test_parse_negative_time() {
+        let now = NaiveTime::from_hms_opt(13, 51, 30).unwrap();
+        let result = try_parse(&to_slice("remind me to do something at 7:10:31"), now);
+        assert!(result.is_err());
     }
 }
