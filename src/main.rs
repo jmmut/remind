f264814be1af90e3faea49b80eba9b6d1478ffe8
jose miguel mut lopez <jose.mut@intrasonics.com>
2023-08-22 14:08:11 +0100

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
+use chrono::NaiveTime;
 use std::process::Command;
 use std::time::Duration;
-use chrono::NaiveTime;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    let args_ref:Vec<&str> = args.iter().map(String::as_str).collect();
+    let args_ref: Vec<&str> = args.iter().map(String::as_str).collect();
     let result = try_parse(&args_ref, chrono::Local::now().time());
     match result {
         Err(e) => println!("Error: {}", e),
@@ -21,7 +21,7 @@ fn main() {
             // println!("status: {}", _output.status);
             // println!("stdout: {}", String::from_utf8_lossy(&_output.stdout));
             // println!("stderr: {}", String::from_utf8_lossy(&_output.stderr));
-        },
+        }
     }
 }
 const ACTION_MARKERS: [&str; 2] = ["to", "that"];
@@ -49,7 +49,7 @@ fn try_parse(words: &[&str], now: NaiveTime) -> Result<Reminder, String> {
         parse_time_action(&words, now, &mut i)
     } else {
         parse_action_time(&words, now, &mut i)
-    }
+    };
 }
 
 fn parse_time_diff_action(words: &&[&str], i: &mut usize) -> Result<Reminder, String> {
@@ -61,10 +61,7 @@ fn parse_time_diff_action(words: &&[&str], i: &mut usize) -> Result<Reminder, St
         *i += 1;
     }
     let action = words[*i..].join(" ");
-    Ok(Reminder {
-        time,
-        action,
-    })
+    Ok(Reminder { time, action })
 }
 
 fn parse_time_action(words: &&[&str], now: NaiveTime, i: &mut usize) -> Result<Reminder, String> {
@@ -76,10 +73,7 @@ fn parse_time_action(words: &&[&str], now: NaiveTime, i: &mut usize) -> Result<R
         *i += 1;
     }
     let action = words[*i..].join(" ");
-    Ok(Reminder {
-        time,
-        action,
-    })
+    Ok(Reminder { time, action })
 }
 
 fn parse_action_time(words: &[&str], now: NaiveTime, i: &mut usize) -> Result<Reminder, String> {
@@ -89,23 +83,25 @@ fn parse_action_time(words: &[&str], now: NaiveTime, i: &mut usize) -> Result<Re
     return if words.len() <= *i {
         Err("Expected an action".to_string())
     } else {
-        let time_diff_index_opt = words.iter().enumerate().rfind(|w| w.1 == &"in").map_or(None, |w| Some(w.0));
+        let time_diff_index_opt = words
+            .iter()
+            .enumerate()
+            .rfind(|w| w.1 == &"in")
+            .map_or(None, |w| Some(w.0));
         if let Some(mut time_index) = &time_diff_index_opt {
             let action = words[*i..time_index].join(" ");
             let time = parse_time_diff(&words, &mut time_index)?;
-            Ok(Reminder {
-                time,
-                action,
-            })
+            Ok(Reminder { time, action })
         } else {
-            let time_index_opt = words.iter().enumerate().rfind(|w| w.1 == &"at").map_or(None, |w| Some(w.0));
+            let time_index_opt = words
+                .iter()
+                .enumerate()
+                .rfind(|w| w.1 == &"at")
+                .map_or(None, |w| Some(w.0));
             if let Some(mut time_index) = &time_index_opt {
                 let action = words[*i..time_index].join(" ");
                 let time = parse_time(&words, &mut time_index, now)?;
-                Ok(Reminder {
-                    time,
-                    action,
-                })
+                Ok(Reminder { time, action })
             } else {
                 let action = words[*i..].join(" ");
                 Ok(Reminder {
@@ -114,7 +110,7 @@ fn parse_action_time(words: &[&str], now: NaiveTime, i: &mut usize) -> Result<Re
                 })
             }
         }
-    }
+    };
 }
 
 fn parse_time_diff(words: &[&str], i: &mut usize) -> Result<Duration, String> {
@@ -161,11 +157,9 @@ fn parse_time(words: &[&str], i: &mut usize, now: NaiveTime) -> Result<Duration,
     }
     let time = match NaiveTime::parse_from_str(words[*i], "%H:%M") {
         Ok(t) => t,
-        Err(_) => {
-            match NaiveTime::parse_from_str(words[*i], "%H:%M:%S") {
-                Ok(t) => t,
-                Err(_) => return Err("Expected a time".to_string()),
-            }
+        Err(_) => match NaiveTime::parse_from_str(words[*i], "%H:%M:%S") {
+            Ok(t) => t,
+            Err(_) => return Err("Expected a time".to_string()),
         },
     };
     *i += 1;
@@ -176,11 +170,10 @@ fn parse_time(words: &[&str], i: &mut usize, now: NaiveTime) -> Result<Duration,
     Ok(Duration::from_secs(time.num_seconds() as u64))
 }
 
-
 #[cfg(test)]
 mod tests {
-    use chrono::NaiveTime;
     use super::*;
+    use chrono::NaiveTime;
 
     fn now() -> NaiveTime {
         return NaiveTime::from_hms_opt(13, 51, 30).unwrap();
@@ -211,66 +204,87 @@ mod tests {
     #[test]
     fn test_parse_only_action() {
         let result = try_parse(&to_slice("remind work"), now());
-        assert_eq!(result, Ok(Reminder {
-            time: Duration::from_secs(0),
-            action: "work".to_string(),
-        }));
+        assert_eq!(
+            result,
+            Ok(Reminder {
+                time: Duration::from_secs(0),
+                action: "work".to_string(),
+            })
+        );
     }
 
     #[test]
     fn test_parse_time_and_action() {
         let result = try_parse(&to_slice("remind in 5 minutes to ask"), now());
-        assert_eq!(result, Ok(Reminder {
-            time: Duration::from_secs(5 * 60),
-            action: "ask".to_string(),
-        }));
+        assert_eq!(
+            result,
+            Ok(Reminder {
+                time: Duration::from_secs(5 * 60),
+                action: "ask".to_string(),
+            })
+        );
     }
 
     #[test]
     fn test_parse_to_action_and_time() {
         let result = try_parse(&to_slice("remind to ask in 5 minutes"), now());
-        assert_eq!(result, Ok(Reminder {
-            time: Duration::from_secs(5 * 60),
-            action: "ask".to_string(),
-        }));
+        assert_eq!(
+            result,
+            Ok(Reminder {
+                time: Duration::from_secs(5 * 60),
+                action: "ask".to_string(),
+            })
+        );
     }
 
     #[test]
     fn test_parse_me_and_action_and_time() {
         let result = try_parse(&to_slice("remind me to ask something in 5 minutes"), now());
-        assert_eq!(result, Ok(Reminder {
-            time: Duration::from_secs(5 * 60),
-            action: "ask something".to_string(),
-        }));
+        assert_eq!(
+            result,
+            Ok(Reminder {
+                time: Duration::from_secs(5 * 60),
+                action: "ask something".to_string(),
+            })
+        );
     }
 
     #[test]
     fn test_parse_action_and_time() {
         let result = try_parse(&to_slice("remind closing something in 5 minutes"), now());
-        assert_eq!(result, Ok(Reminder {
-            time: Duration::from_secs(5 * 60),
-            action: "closing something".to_string(),
-        }));
+        assert_eq!(
+            result,
+            Ok(Reminder {
+                time: Duration::from_secs(5 * 60),
+                action: "closing something".to_string(),
+            })
+        );
     }
 
     #[test]
     fn test_parse_action_and_at_time() {
         let now = NaiveTime::from_hms_opt(13, 51, 30).unwrap();
         let result = try_parse(&to_slice("remind me to do something at 14:10"), now);
-        assert_eq!(result, Ok(Reminder {
-            time: Duration::from_secs(18 * 60 + 30),
-            action: "do something".to_string(),
-        }));
+        assert_eq!(
+            result,
+            Ok(Reminder {
+                time: Duration::from_secs(18 * 60 + 30),
+                action: "do something".to_string(),
+            })
+        );
     }
 
     #[test]
     fn test_parse_action_and_at_time_seconds() {
         let now = NaiveTime::from_hms_opt(13, 51, 30).unwrap();
         let result = try_parse(&to_slice("remind me to do something at 14:10:31"), now);
-        assert_eq!(result, Ok(Reminder {
-            time: Duration::from_secs(19 * 60 + 1),
-            action: "do something".to_string(),
-        }));
+        assert_eq!(
+            result,
+            Ok(Reminder {
+                time: Duration::from_secs(19 * 60 + 1),
+                action: "do something".to_string(),
+            })
+        );
     }
 
     #[test]
